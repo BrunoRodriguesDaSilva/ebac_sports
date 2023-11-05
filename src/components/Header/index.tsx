a86a311ba00adba39dceeb4f679699b268272792
Bruno Rodrigues da Silva <Brunochh@outlook.com>
2023-11-05 13:36:59 -0300

    enviodo exercicio modulo_30

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
-import * as S from './styles'
-
-import { Produto } from '../../App'
-
-import cesta from '../../assets/cesta.png'
+import { RootReducer } from '../../store'
+import { useSelector } from 'react-redux'
 import { paraReal } from '../Produto'
+import * as S from './styles'
+import cesta from '../../assets/cesta.png'
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
+const Header = () => {
+  const itensCarrinho = useSelector(
+    (state: RootReducer) => state.carrinho.itens
+  )
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favorito.itens
+  )
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+  const valorTotal = itensCarrinho.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -20,10 +21,10 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
     <S.Header>
       <h1>EBAC Sports</h1>
       <div>
-        <span>{favoritos.length} favoritos</span>
+        <span>{itensFavoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itensCarrinho.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,24 +1,18 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
-
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../store'
 import * as S from './styles'
+import { useGetProdutosQuery } from '../services/api'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
-
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = () => {
+  const { data: produtos } = useGetProdutosQuery()
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favorito.itens
+  )
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    const IdsDosFavoritos = itensFavoritos.map((f) => f.id)
 
     return IdsDosFavoritos.includes(produtoId)
   }
@@ -26,13 +20,11 @@ const ProdutosComponent = ({
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
